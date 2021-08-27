f7eef4fd6280890ce7804f332be706fa7f1093b8
animemandir <animemandir40@gmail.com>
2021-08-27 12:02:57 +0530

    Updates

---
+++ precache-manifest.0d599c34fd3ae1fcf3275ee7dc2618c2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d663b07ecff8fe93c77",
+    "url": "/pokedex/static/css/main.48dda17c.chunk.css"
+  },
+  {
+    "revision": "9d663b07ecff8fe93c77",
+    "url": "/pokedex/static/js/main.286f292c.chunk.js"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  },
+  {
+    "revision": "19ab00b7054b75ede33d",
+    "url": "/pokedex/static/css/2.4725dfe9.chunk.css"
+  },
+  {
+    "revision": "19ab00b7054b75ede33d",
+    "url": "/pokedex/static/js/2.f21c4f54.chunk.js"
+  },
+  {
+    "revision": "1783cfb665c6bf50271c886d7d255b98",
+    "url": "/pokedex/static/media/pokedex.1783cfb6.png"
+  },
+  {
+    "revision": "edf5c25f2721b8f162491ce2db89ee08",
+    "url": "/pokedex/static/media/bg.edf5c25f.png"
+  },
+  {
+    "revision": "95a3fbc446c57bfa239f0a52d5859014",
+    "url": "/pokedex/static/media/BarcadeBrawl.95a3fbc4.ttf"
+  },
+  {
+    "revision": "dc0354529eb5078ad826b18cc56a7443",
+    "url": "/pokedex/index.html"
+  }
+];
